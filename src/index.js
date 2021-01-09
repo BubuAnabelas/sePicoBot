df55b30fa66872a2afc513c07a463bcccd62b22f
Ieltxu Algañaras <ieltxu.a@ag2technology.com>
2021-01-09 00:23:57 -0300

    try express server on PORT

--- src/index.js
+++ src/index.js
@@ -10,10 +10,16 @@ db.once('open', function () {
   console.log('conectado');
 });
 
-// const express = require('express');
-// const path = require('path');
-// const app = express();
+const express = require('express');
+const app = express();
 
+app.listen(process.env.PORT, () =>
+  console.log('Gator app listening on port !' + process.env.PORT)
+);
+
+app.get('/', (req, res) => {
+  res.send('Se Pico Bot');
+});
 const options = {
   options: {
     debug: false,
