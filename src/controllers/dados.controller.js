9b484056a7f0c338c2ff46bee9bf473610139a68
ieltxu <ieltxu.a@ag2technology.com>
2021-04-05 02:53:10 -0300

    fix

--- src/controllers/dados.controller.js
+++ src/controllers/dados.controller.js
@@ -2,12 +2,13 @@ export const dados = (client, args, channel, tags, message, self) => {
     let dados = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣"];
     let dadoUno = dados[Math.floor(Math.random() * dados.length)];
     let dadoDos = dados[Math.floor(Math.random() * dados.length)];
+    let dadoTres, dadoCuatro;
     if (dadoUno === dadoDos) {
-        let dadoTres = dados[Math.floor(Math.random() * dados.length)];
-        let dadoCuatro = dados[Math.floor(Math.random() * dados.length)];
+        dadoTres = dados[Math.floor(Math.random() * dados.length)];
+        dadoCuatro = dados[Math.floor(Math.random() * dados.length)];
     }
     client.say(
         channel,
-        `@${tags.username} tiraste ${dadoUno} ${dadoDos} ${dadoTres ? "y " + dadoTres + " " + dadoCuatro : ""} }`
+        `@${tags.username} tiraste ${dadoUno} ${dadoDos} y ${dadoTres ? "y " + dadoTres + " " + dadoCuatro : ""} }`
     );
 }
