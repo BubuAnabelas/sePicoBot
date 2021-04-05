383ea8cc187200ef98973a805d922f4d2c40fcdb
ieltxu <ieltxu.a@ag2technology.com>
2021-04-05 02:42:25 -0300

    doble dado

--- src/controllers/dados.controller.js
+++ src/controllers/dados.controller.js
@@ -2,8 +2,12 @@ export const dados = (client, args, channel, tags, message, self) => {
     let dados = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣"];
     let dadoUno = dados[Math.floor(Math.random() * dados.length)];
     let dadoDos = dados[Math.floor(Math.random() * dados.length)];
+    if (dadoUno === dadoDos) {
+        let dadoTres = dados[Math.floor(Math.random() * dados.length)];
+        let dadoCuatro = dados[Math.floor(Math.random() * dados.length)];
+    }
     client.say(
         channel,
-        `@${tags.username} tiraste ${dadoUno} ${dadoDos}`
+        `@${tags.username} tiraste ${dadoUno} ${dadoDos} ${dadoTres ? "y " + dadoTres + " " + dadoCuatro : ""} }`
     );
 }
