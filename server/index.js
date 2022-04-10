4d6b4ba4c6cd03d4b79bf0a734b15f00a18ac8e4
Diego Duarte <die.duarte@gmail.com>
2022-04-10 03:41:24 -0300

    testing if canal variable of .env file is not working in prod

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const canalDeTwitch = process.env.canal;
+const canalDeTwitch = "jmellera";
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
